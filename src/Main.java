f41c2994e353467414fc478a3986eb63ce9fa4c8
longjianwei613@163.com <ljw123456>
2021-03-17 01:01:28 +0800

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+public class Main {
+    public static void main(String[] args) {
+        HashMap map = new HashMap();
+        map.put("1","a");
+        System.out.print(map);
+    }
+}
